e25bc2648ec6b7dc4cc4c3981bee3816788c3688
Gus <gus_eze1988@hotmail.com>
2023-02-12 20:45:24 -0300

    Termina clase 1 de Multer

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,10 +1,25 @@
+const {readJSON, writeJSON} = require("../data")
+
 module.exports = {
     addOneImage : (req, res) => {
         return res.render("addOneImage")
     },
 
     storeOneImage : (req, res) => {
-        return res.send(req.body)
+        const products = readJSON("productsOneImage.json");
+
+        const newProduct = {
+            id : products.length ? products[products.length - 1].id + 1 : 1,
+            name : req.body.name,
+            image : req.file ? req.file.filename : null,
+            description : "lorem impsum dolor amet sit"
+        }
+
+        products.push(newProduct)
+
+        writeJSON("productsOneImage.json", products)
+
+        return res.redirect("/")
     },
 
     detailOneImage : (req, res) => {
--- routes/products.js
+++ routes/products.js
@@ -3,10 +3,13 @@ const router = express.Router();
 
 const {addOneImage, storeOneImage, detailOneImage} = require("../controllers/productsController")
 
+const {upLoadOneImage} = require("../middlewares/upLoad")
+
+
 /* /products */ 
 router
     .get('/add-one-image', addOneImage)
-    .post('/add-one-image', storeOneImage)
+    .post('/add-one-image', upLoadOneImage.single("image"), storeOneImage)
     .get('/detail-one-image', detailOneImage)
 
 module.exports = router;
