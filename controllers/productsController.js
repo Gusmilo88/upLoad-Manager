a7c2047d7b5c791ddf19b71d807325e0a0682e96
Gus <gus_eze1988@hotmail.com>
2023-02-13 01:18:18 -0300

    Termina clase 2 de Multer

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -29,7 +29,7 @@ module.exports = {
 
         return res.render("detailOneImage", {
             ...product
-        })
+        });
     },
 
     addMultipleImages : (req, res) => {
@@ -37,12 +37,40 @@ module.exports = {
     },
 
     storeMultipleImages : (req, res) => {
-        return res.send(req.body)
+        const products = readJSON("productsMultipleImages.json");
 
+        const newProduct = {
+            id : products.length ? products[products.length - 1].id + 1 : 1,
+            name : req.body.name,
+            images : req.files.map(file => file.filename),
+            description : "lorem impsum dolor amet sit"
+        };
+
+        products.push(newProduct);
+
+        writeJSON("productsMultipleImages.json", products);
+        return res.redirect("/");
     },
 
     detailMultipleImages : (req, res) => {
-        return res.render("detailMultipleImages")
+        const products = readJSON("productsMultipleImages.json");
+        const product = products.find(product => product.id === +req.params.id)
 
+        return res.render("detailMultipleImages", {
+            ...product
+        });
+
+    },
+
+    addMainImage : (req, res) => {
+        return res.render("addMainImage")
+    },
+
+    storeMainImage : (req, res) => {
+        return res.send(req.body)
+    },
+
+    detailMainImage : (req, res) => {
+        return res.render("detailMainImage")
     },
 }
--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const {addOneImage, storeOneImage, detailOneImage, addMultipleImages, storeMultipleImages, detailMultipleImages} = require("../controllers/productsController")
+const {addOneImage, storeOneImage, detailOneImage, addMultipleImages, storeMultipleImages, detailMultipleImages, addMainImage, storeMainImage, detailMainImage} = require("../controllers/productsController")
 
 const {upLoadProductImages} = require("../middlewares/upLoad")
 
@@ -15,5 +15,9 @@ router
     .get('/add-multiple-images', addMultipleImages)
     .post('/add-multiple-images', upLoadProductImages.array("images"), storeMultipleImages)
     .get('/detail-multiple-images/:id', detailMultipleImages)
+
+    .get('/add-main-image', addMainImage)
+    .post('/add-main-image', storeMainImage)
+    .get('/detail-main-image/:id', detailMainImage)
 
 module.exports = router;
