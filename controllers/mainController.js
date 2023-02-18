5b257fb71e6e3b228941a37f65a987c8b2a8c898
Gus <gus_eze1988@hotmail.com>
2023-02-18 00:12:49 -0300

    Clase 74 multer parte 3

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -2,12 +2,15 @@ const { readJSON } = require("../data")
 
 module.exports = {
     home : (req, res) => {
-        let productsOneImage = readJSON("productsOneImage.json")
-        let productsMultipleImages = readJSON("productsMultipleImages.json")
+        let productsOneImage = readJSON("productsOneImage.json");
+        let productsMultipleImages = readJSON("productsMultipleImages.json");
+        let productsMainImage = readJSON("productsMainImage.json")
+
 
         return res.render("index", {
             productsOneImage,
-            productsMultipleImages
+            productsMultipleImages,
+            productsMainImage
         })
     }
 }
